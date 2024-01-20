43a55f8466950fdb196d6f8c84c0e9c0ee703e3b
devlim <ywl428@naver.com>
2024-01-20 15:38:19 +0900

    N.7-2

--- react-basic/src/App.tsx
+++ react-basic/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import TestComponent, { testVariable } from './components/TestComponent';
 import './style.css';
@@ -6,14 +7,56 @@ import Header from './views/Layout/Header';
 import Main from './views/Layout/Main';
 
 function App() {
+
+  const [count, setCount] = useState<number>(0);
+
+  // let count = 0;
+
+  const increaseCount = () => {
+    // count++;
+    const result = count + 1;
+    setCount(result)
+    // alert(result);
+  }
+
+  // useEffect : 함수형 컴포넌트가 react DOM 마운트되고 (화면에 출력되고)
+  //             호출되는 함수
+  // useEffect(콜백함수, 배열);
+
+  let flag = false;
+
+  // 1. 첫 랜더링 직후 실행할 코드를 지정 (두 번 실행)
+  useEffect(() => {
+    // useEffect를 한번만 실행하게 도와주는 로직
+    if (!flag) {
+      flag = true;
+      return;
+    }
+    // alert('useEffect 실행!');
+  }, []);
   
+  // 2. 특정 상태가 변경된 후 실행할 코드를 지정
+  useEffect(() => {
+    // 주의사항 : 해당 useEffect로 스코프하고 있는 상태를 내부에서 변경하면 무한 호출됨
+    // setCount(count + 1);
+    // alert('count 상태 변경');
+  }, [count]);
+
+  // 3. 화면이 종료될때 실행할 코드를 지정
+  useEffect(() => {
+    return () => {
+      // alert('화면 종료');
+    }
+  }, []);
+
   return (
     <>
       {/* 
         // <TestComponent arg1={'문자열'} /> 
         // <TestComponent arg1={'문자열'} arg2={100} />
       */}
-      <Header count={0}/>
+      <button onClick={increaseCount}>+</button>
+      <Header count={count}/>
       <Main />
       <Footer />
     </>
--- react-basic/src/views/Layout/Footer/index.tsx
+++ react-basic/src/views/Layout/Footer/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+interface Human {
+  name: string;
+  age: number;
+}
 
 export default function Footer() {
+
+  const[human, setHuman] = useState<Human>({ name: '홍길동', age: 20 });
+
+  const changeHuman = () => {
+    human.name = '김철수';
+    human.age = 30;
+    setHuman(human);
+  }
+
   return (
-    <div>Footer</div>
+    <div>
+      <div>{human.name} {human.age}</div>
+      <button onClick={changeHuman}>변경</button>
+    </div>
   )
 }
